07455ac78db5208a87ab9692dc26e237767eed8b
HugoJavid <Hugojavidferrero@gmail.com>
2023-01-30 10:34:33 +0100

    prueba1

--- src/Prueba1/Prueba1.java
+++ src/Prueba1/Prueba1.java
@@ -1,5 +1,11 @@
 package Prueba1;
 
 public class Prueba1 {
+public static void main(String[] args) {
+	System.out.println("hola mundo soy java-git");
+	System.out.println("hola mundo soy java-git1");
+	System.out.println("Adios");
 
+
+}
 }
